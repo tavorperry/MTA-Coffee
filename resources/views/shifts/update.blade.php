d79e2b3d1055d115f8346e21614f90c8aade9b54
cygelman <33072785+cygelman@users.noreply.github.com>
2018-03-30 20:53:29 +0300

    fix: Responsive shift tables

--- resources/views/shifts/update.blade.php
+++ resources/views/shifts/update.blade.php
@@ -4,11 +4,15 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <title></title>
         <style>
-            table, th, td {
-                border: 1px solid black;
-                padding: 5px;
+            table {
+                /*border: 2px solid black;*/
+            }
+            th, td {
+                /*border: 1px solid black;*/
+                /*padding: 5px;*/
                 text-align: center;
             }
+
             input[type='checkbox']{
                 zoom: 2;
             }
@@ -28,30 +32,32 @@
         <form action="{{ route('station.shifts', $station->id) }}" method="POST">
             @csrf
             @method('PUT')
-            <table style="border: 2px solid black">
-                <tr>
-                    <th>משמרת</th>
-                    <th>ראשון</th>
-                    <th>שני</th>
-                    <th>שלישי</th>
-                    <th>רביעי</th>
-                    <th>חמישי</th>
-                </tr>
-                <tr>
-                    <th>בוקר 8:00-14:00<br></th>
-                    @foreach($station->shifts as $shift)
-                        <td><input type="checkbox" name="shifts[]" value="{{ $shift->id }}"></td>
-                        @break($loop->index == 4)
-                    @endforeach
-                </tr>
-                <tr>
-                    <th>ערב 14:00-20:00<br></th>
-                    @for($i=5; $i < 10; $i++)
-                        <td><input type="checkbox" name="shifts[]" value="{{ $station->shifts[$i]->id }}"></td>
-                    @endfor
-                </tr>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <tr>
+                        <th>משמרת</th>
+                        <th>ראשון</th>
+                        <th>שני</th>
+                        <th>שלישי</th>
+                        <th>רביעי</th>
+                        <th>חמישי</th>
+                    </tr>
+                    <tr>
+                        <th>בוקר 8:00-14:00<br></th>
+                        @foreach($station->shifts as $shift)
+                            <td><input type="checkbox" name="shifts[]" value="{{ $shift->id }}"></td>
+                            @break($loop->index == 4)
+                        @endforeach
+                    </tr>
+                    <tr>
+                        <th>ערב 14:00-20:00<br></th>
+                        @for($i=5; $i < 10; $i++)
+                            <td><input type="checkbox" name="shifts[]" value="{{ $station->shifts[$i]->id }}"></td>
+                        @endfor
+                    </tr>
 
-            </table><br>
+                </table><br>
+            </div>
             <button type='submit'>עדכן משמרות</button>
         </form>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
