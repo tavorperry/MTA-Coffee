d77b7b60a28b99c53c17e53b52eda8d69cbcb707
Almog Cygel <cygelman@gmail.com>
2018-05-12 20:21:37 +0300

    fix: Register user to DB fixed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'first_name' => $data['first_name'],
-            'last_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
 
