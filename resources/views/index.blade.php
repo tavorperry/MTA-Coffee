a0a35065ee54c2529aeaad588a8a383139560338
Almog Cygel <cygelman@gmail.com>
2018-05-22 21:16:33 +0300

    update: rewrite text in index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -63,7 +63,7 @@
             </a>
         </form>
     @else
-        <h2 class="service-description text-center">השירותים שלנו - לחץ וקבל הסבר על כל שירות :)</h2>
+        <h2 class="service-description text-center">לחץ וקבל הסבר על כל שירות :)</h2>
         <div class="row">
             <button type="button" href="{{ route('notifications.show') }}" class="btn menu-btn col" data-trigger="focus" data-container="body" data-toggle="popover" data-placement="top" data-content="קבל התרעות מדיווחים של משתמשים אחרים וקבל נקודות!"> <i class="far fa-flag menu-btn-icon"></i><br>
                 התראות
