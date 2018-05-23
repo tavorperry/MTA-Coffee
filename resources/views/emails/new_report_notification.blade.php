f6f0b3b9780272f86c593ecf312b66ec1342aedc
Perry <tavorp12@gmail.com>
2018-05-23 18:46:08 +0300

    fix: RTL in Email template

--- resources/views/emails/new_report_notification.blade.php
+++ resources/views/emails/new_report_notification.blade.php
@@ -5,6 +5,9 @@
         .blue {
             color: blue;
         }
+        *{
+            text-align: right;
+        }
     </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
