23a506508cf7fefd0ebb4072e0956e82455dedb8
Perry <tavorp12@gmail.com>
2018-06-10 00:36:22 +0300

    fix: delete comments

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -8,10 +8,8 @@ use Illuminate\Http\Request;
 class loginController extends Controller
 {
     public function getUser(){
-       // $name = ($_POST['f_name']);
         $users = \App\User::all();
         return view('login', compact('users'));
-       // var_dump($user->name);
 
     }
 }
