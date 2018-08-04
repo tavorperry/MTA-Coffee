ed6be058314f233e7cc70116acb6ac782930719f
Tavor <tavorp12@gmail.com>
2018-08-04 06:19:07 +0300

    New design to create report page

--- resources/views/reports/create.blade.php
+++ resources/views/reports/create.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <h1 class="service-description text-center">דווח על בעיה בעמדה</h1>
+    <h2 class="service-description text-center">דווח על בעיה בעמדה</h2>
     <hr>
     @if(session('message'))
         <div>{{ session('message') }}</div>
@@ -13,39 +13,52 @@
     {{ Form::open(['route' => ['reports.store'],'files' => true]) }}
         @csrf
 
-        <div class="text-center">
-            <label><h3 class="service-description">עמדה</h3></label><br>
-            <select  class="form-control-lg" id="check" name="station">
+        <div class="row">
+            <label class="to-the-right col-3"><h4 class="service-description">עמדה</h4></label>
+            <select  class="form-control col-9" id="check" name="station">
                 <option value="1">פומנטו</option>
                 <option value="2">ווסטון</option>
                 <option value="3">כלכלה</option>
             </select>
         </div>
-        <br>
-        <div class=" text-center">
-            <label><h3 class="service-description">סוג</h3></label><br>
-            <select class="form-control-lg" name="type">
+        <div class="row">
+            <label class="to-the-right col-3"><h4 class="service-description">סוג</h4></label>
+            <select class="form-control col-9" name="type">
                 <option value="חסר משהו">חסר משהו</option>
                 <option value="יש תקלה">יש תקלה</option>
                 <option value="לא נקי">לא נקי</option>
                 <option value="אחר">אחר</option>
             </select>
         </div>
-        <br>
-        <div class="text-center">
-            <label><h3 class="service-description">פרטים נוספים</h3></label><br>
-            <textarea rows="4" cols="30" name="message" placeholder="נא לשמור על שפה תקינה, עד 50 תווים"></textarea><br>
-            <span id="characters">0</span><span>/50</span>
+        <br><br>
+        <div class="row">
+            <label class="to-the-right col-3"><h4 class="service-description">פרטים נוספים</h4></label>
+            <textarea rows="4" cols="30" id="message" type="text" class="col-9 form-control{{ $errors->has('message') ? ' is-invalid' : '' }}" name="message" value="{{ old('message') }}" required placeholder="פרטו בקצרה על הליקוי, עד 50 תווים"></textarea>
+            @if ($errors->has('message'))
+                <span class="invalid-feedback" style="display: block !important;">
+                    <strong>אנא הקלידו מעט פרטים כדי שהמתנדבים שלנו יוכלו לעזור :)</strong>
+                </span>
+            @endif
         </div>
-        <br>
-        <h3 class="service-description text-center">הוסף תמונה</h3>
-        <div class="input-group mb-3">
+    <div class="text-center">
+        <span id="characters">0</span><span>/50</span>
+    </div>
+<br>
+    <div class="row">
+        <h4 class="service-description to-the-right col-3">הוסף תמונה</h4>
+        <div class="input-group mb-3 col-9">
             <div class="custom-file">
-                <input type="file" class="custom-file-input" name="picture" id="inputGroupFile01" onchange="$('#upload-file-info').replaceWith('<b>!File selected</b>');">
+                    <input id="inputGroupFile01" type="file" class="custom-file-input form-control{{ $errors->has('picture') ? ' is-invalid' : '' }}" name="picture" value="{{ old('picture') }}" autofocus onchange="$('#upload-file-info').replaceWith('<b>!File selected</b>');">>
                 <label class="custom-file-label text-left" for="inputGroupFile01"><span id="upload-file-info"></span></label>
             </div>
+            @if ($errors->has('picture'))
+                <span class="invalid-feedback" style="display: block !important;">
+                    <strong>{{ $errors->first('picture') }}</strong>
+                </span>
+            @endif
         </div>
-        <br>
+    </div>
+    <br>
         <div class="container">
             <div class="text-center">
                 {{--<input type="submit" value="דווח!">--}}
