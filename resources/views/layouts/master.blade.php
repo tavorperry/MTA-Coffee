bb5b96698d4ec7913961008d44b87bef9438b792
Tavor <tavorp12@gmail.com>
2018-10-06 02:52:10 +0300

    change css's links to make sure HTTPS redirects all files(#3)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
             <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css" rel="stylesheet">
             <link href="https://fonts.googleapis.com/css?family=Assistant" rel="stylesheet">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
-            <link href="{{env('APP_URL')/css/index.css}}" media="all" rel="stylesheet" type="text/css" />
+            <link href="{{env('APP_URL')}}/css/index.css" media="all" rel="stylesheet" type="text/css" />
             <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Varela+Round" rel="stylesheet">
             <meta name="csrf-token" content="{{ csrf_token() }}">
             @yield('page-style')
