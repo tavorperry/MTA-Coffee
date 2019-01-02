b02ae97aa9a7f8821dbe2b07fecedbca58cdffaa
Tavor <tavorp12@gmail.com>
2019-01-02 02:23:30 +0200

    PWA - Beta 0.1

---
+++ config/laravelpwa.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'name' => 'MTA-Coffee_PWA',
+    'manifest' => [
+        'name' => env('APP_NAME', 'MTA_COFFEE'),
+        'short_name' => 'MTAC',
+        'start_url' => '/',
+        'background_color' => '#ffffff',
+        'theme_color' => '#000000',
+        'display' => 'standalone',
+        'orientation'=> 'any',
+        'icons' => [
+            '72x72' => 'images/icons/icon-72x72.png',
+            '96x96' => 'images/icons/icon-96x96.png',
+            '128x128' => 'images/icons/icon-128x128.png',
+            '144x144' => 'images/icons/icon-144x144.png',
+            '152x152' => 'images/icons/icon-152x152.png',
+            '192x192' => 'images/icons/icon-192x192.png',
+            '384x384' => 'images/icons/icon-384x384.png',
+            '512x512' => 'images/icons/icon-512x512.png',
+        ]
+    ]
+];
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html dir="rtl">
 <head>
+    @laravelPWA
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
