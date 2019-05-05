623714fea61d07727242fbe5dae905288a5eef60
Tavor <tavorp12@gmail.com>
2019-05-05 21:48:26 +0300

    view

--- resources/views/machine/get_coffee.blade.php
+++ resources/views/machine/get_coffee.blade.php
@@ -3,7 +3,7 @@
 @endsection
 
 @section('content')
-    <h2 class="service-description text-center">הטענת ארנק דיגיטלי בכרטיס אשראי</h2>
+    <h2 class="service-description text-center">פתיחת מכונה - קניית קפה</h2>
     <hr>
     @if(session('message'))
         <div>{{ session('message') }}</div>
@@ -12,12 +12,12 @@
     @csrf
     <div class="row">
         <label class="to-the-right col-3"><h4 class="service-description">בחר מכונה</h4></label>
-        <select id="month" name="month" class="col-9 form-control{{ $errors->has('machine') ? ' is-invalid' : '' }}" value="{{ old('machine') }}" required>
+        <select id="machineNumber" name="machineNumber" class="col-9 form-control{{ $errors->has('machineNumber') ? ' is-invalid' : '' }}" value="{{ old('machineNumber') }}" required>
             <option value="2">ווסטון</option>
         </select>
     </div>
 
-    <div class="row">
+    {{--<div class="row">
         <label class="to-the-right col-3"><h4 class="service-description">הערות</h4></label>
         <input id="comment" name="comment" type="text" maxlength="50" class="col-9 form-control" placeholder="הערות (עד 50 תווים)" >
 
@@ -27,11 +27,11 @@
                     <strong>אנא הקלידו הערה תקינה. בלי תווים מיוחדים ועד 100 תווים</strong>
                 </span>
         @endif
-    </div>
+    </div>--}}
     <br>
     <div class="container">
         <div class="text-center">
-            {!! app('captcha')->render(); !!}
+           {{-- {!! app('captcha')->render(); !!}--}}
             <button type="submit" class="btn login-btn" value="דווח!">הטען!</button>
         </div>
     </div>
