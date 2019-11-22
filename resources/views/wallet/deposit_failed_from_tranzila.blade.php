b845e272b6e0e0dc769a15394d3bdf98003a443f
Tavor <tavorp12@gmail.com>
2019-11-23 01:53:24 +0200

    .

--- resources/views/wallet/deposit_failed_from_tranzila.blade.php
+++ resources/views/wallet/deposit_failed_from_tranzila.blade.php
@@ -16,6 +16,8 @@
             <link href="{{env('APP_URL')}}/css/index.css" media="all" rel="stylesheet" type="text/css" />
             <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Varela+Round" rel="stylesheet">
             <script type="text/javascript" src="{{env('APP_URL')}}/js/index.js"></script>
+            <!-- jQuery library -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
             <meta name="csrf-token" content="{{ csrf_token() }}">
             @yield('page-style')
             <title>MTAcoffee</title>
